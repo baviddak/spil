b43baad49e03258e3199a5fe2a1c98d00804f86e
David Bak <dbak.db@gmail.com>
2026-07-12 14:23:44 -0400

    save progress

---
+++ 11_proc_creation/ex04/main.c
@@ -0,0 +1,81 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+
+#define MAX_CHILDREN 128
+
+struct child_list {
+	pid_t children[MAX_CHILDREN];
+	int size;
+};
+
+void add_list_child(pid_t child_pid, struct child_list *ch_list) {
+
+	if ( ch_list->size == MAX_CHILDREN - 1 ) {
+		fprintf(stderr, "Too many children created!\n");
+		exit(EXIT_FAILURE);
+	}
+
+	ch_list->children[ch_list->size] = child_pid;
+	ch_list->size++;
+	return;
+}
+
+
+int main (int argc, char *argv[]) {
+
+	long val;
+	pid_t current_pid;
+
+	if ( argc != 2 || strcmp(argv[1], "--help") == 0 ) {
+		fprintf(stderr, "Usage: ./cascade N\n\nWhere N = number of processes. N must be less than or equal to 128.\n");
+		exit(EXIT_FAILURE);
+	}
+
+	errno = 0;
+	val = strtol(argv[1], NULL, 10);
+
+	if ( errno == ERANGE ) {
+		fprintf(stderr, "%s is not a valid number: %s\n", argv[1], strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+
+	if ( val > 128 || val < 0 ) {
+		fprintf(stderr, "%ld is not a valid number\n", val);
+		exit(EXIT_FAILURE);
+	}
+
+	struct child_list new_cl;
+	new_cl.size = 0;
+
+	for (int i = 0; i < val; i++ ) {
+		switch ( (current_pid = fork() )) {
+			case -1:
+				fprintf(stderr, "Error in fork(): %s\n", strerror(errno));
+				exit(EXIT_FAILURE);
+				break;
+			case 0:
+				/* Child process */
+				fprintf(stdout, "PID child %d: %d\n", i, getpid());
+				pause();
+				break;
+			default:
+				/* Parent process */
+				add_list_child(current_pid, &new_cl);
+				break;
+		}
+
+	}
+
+	for (int i = 0; i < new_cl.size; i++ ) {
+		fprintf(stdout, "The pid is %d\n", new_cl.children[i]);
+	}
+
+	/*
+	 * Work in progress
+	 */
+
+	exit(EXIT_SUCCESS);
+}
